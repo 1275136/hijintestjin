7b1e0d74069369d24452d681294eab1c8fdcabfc
jin <42559373+1275136@users.noreply.github.com>
2022-06-12 15:41:12 -0400

    Create blu.lua

---
+++ blu.lua
@@ -0,0 +1,95 @@
+-- Sheet for Rondo Alla Turca by Wolfgang Amadeus Mozart
+
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 3*60 + 58 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- tempo overides the delay
+
+shared.scr = [[
+[sx] [fh] [fh]
+[sk] [fh] [fh]
+[sj] [gh] k [ghl]
+[sv] [gh] [gh]
+[sx] [fh] [fh]
+[sk] [fh] [fh]
+[sj] [gh] k [ghl]
+[sn] [gh] [gh]
+[sx] [fh] [fh]
+[sk] [fhv] [fhb]
+[sj] [gh] k [ghl]
+[gb] [fv] [dc]
+[gx] c x
+[hz] l
+l h g
+f [8of] [0w] [0wd]
+[7of] [0w] g [0wh]
+[6fl] [80] [80]
+5 [8dk] [8fl]
+[4dk] [89] [8sj]
+[3ah] [9w] [9wg]
+[2of] [68] g [2f]
+[5od] [5if]
+[6us] [0w] [0wa]
+[5is] [qw] d [qwf]
+[5uk] [0w] [5yj]
+[5tf] s
+[4s]
+$ [Qf] [4s]
+[5of] g f
+[59d] w 9
+[8of] [0w] [0wd]
+[7of] [0w] g [0wh]
+[6fl] [80] [80]
+5 [8dk] [8fl]
+[4dk] [89] [8sj]
+[3ah] [9w] [9wg]
+[2of] [68] g [2f]
+[5od] [5if]
+[6us] [0w] [0wa]
+[5is] [qw] d [qwf]
+[5fk] [0w] [5dj]
+[4sf] s
+[4s] $ [Qa] [5s]
+[5of] g f
+[5id] w [9x] z
+[el] t [uh]
+[wx] t c [uz] l
+[ql] t u
+q t [ux] z
+[el] t [uh]
+[wx] t c [uz] l
+[ql] [tm] [un]
+[qv] [tx] [uhz]
+[elx] t [uh]
+[wx] t c [uz] l
+[ql] [tl] [uz]
+[qx] [tc] [uhv]
+[ejx] t [uh]
+[wx] t c [uz] l
+[9l] Q [tj]
+[Qx] t z [ul] x
+[wz] t y
+[wv] [kx]
+[9lc] w [zv] [txb]
+[rzv] t y
+0 [wh] [th]
+[yh] g f g
+[qh] e [tl]
+[yh]
+0 [wh] [th]
+[yh] g f g
+[wh] [td] [ya]
+[us]
+0 [wh] [th]
+[yh] g f g
+[qh] e [tl]
+[yh] s d
+[9g] [qf] [es]
+[wo] s a
+[es] [ux] z [tl] z
+[wx] l z
+[gjc] x l
+h l k
+
+]]
